05fe88d889bf7dcc7260c604355a35371b97a11c
kakaziny <133366438+kakaziny@users.noreply.github.com>
2023-08-04 16:37:08 -0300

    projetoverificacao

---
+++ sorteio/aula.js
@@ -0,0 +1,13 @@
+console.log(Math.random());
+console.log(Math.random() * 20);
+console.log(Math.random(7 - 3 + 1)+3);
+
+console.log(Math.round (3.1));
+console.log(Math.round (3.9)) ;
+
+
+console.log(Math.floor (3.1));
+console.log(Math.floor (3.9));
+
+console.log(Math.ceil (3.1));
+console.log(Math.ceil (3.9));
